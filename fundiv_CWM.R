0440ded64cf665eb2c91633c348864f32cf7f0a2
Pedro Rebollo <pedrorebollo13@gmail.com>
2023-05-17 11:36:47 +0200

    CWM x ifn y spp

--- fundiv_CWM.R
+++ fundiv_CWM.R
@@ -16,22 +16,27 @@ tree34i <- tree34[c("Plotcode", "especie_fin")]
 names(traits_MAD) <- c("especie_ini", "Name", "Max_height", "Wood_density", "LMA", "Seed_mass")
 
 ###tengo que cambiarle los id a los codigos de las especies en la base de datos de traits
-
+library(stringr)
 tree23i$especie_ini <- str_pad(tree23i$especie_ini, 3, pad = "0")
 tree23i$especie_fin <- str_pad(tree23i$especie_fin, 3, pad = "0")
 tree34i$especie_fin <- str_pad(tree34i$especie_fin, 3, pad = "0")
 
 
+##ifn2
 cwm2 <- merge(tree23i, traits_MAD, by="especie_ini", all.x = T) 
-##ahora tendria que agregar cada trait x plot haciendo la media
+cwm2 <- na.omit(cwm2)
+cwm2_plot <- aggregate(cbind(Max_height, Wood_density, LMA, Seed_mass) ~ Plotcode, data = cwm2, FUN = mean, na.rm = F)
 
+##ifn3
 names(traits_MAD) <- c("especie_fin", "Name", "Max_height", "Wood_density", "LMA", "Seed_mass")
 cwm3 <- merge(tree23i, traits_MAD, by="especie_fin", all.x = T) 
-##agregar trait x plot
+cwm3 <- na.omit(cwm3)
+cwm3_plot <- aggregate(cbind(Max_height, Wood_density, LMA, Seed_mass) ~ Plotcode, data = cwm3, FUN = mean, na.rm = F)
 
-
+##ifn4
 cwm4 <- merge(tree34i, traits_MAD, by="especie_fin", all.x = T) 
-##agregar trait x plot
+cwm4 <- na.omit(cwm4)
+cwm4_plot <- aggregate(cbind(Max_height, Wood_density, LMA, Seed_mass) ~ Plotcode, data = cwm4, FUN = mean, na.rm = F)
 
 
 
