67a1654380f0ba4b8478274c5c4fc2d27db0d23a
Pedro Rebollo <pedrorebollo13@gmail.com>
2023-05-29 12:30:07 +0200

    correlations

--- exploratory.R
+++ exploratory.R
@@ -4,16 +4,31 @@ library(ggplot2)
 library(patchwork)
 library(ggpubr) 
 library(GGally)
-
-
-
-
-
+library(ggcorrplot)
+library(ragg)
 
 
 vexp <- dat[, 5:14]
-pairplots <- ggpairs(vexp) 
-pairplots
+corr <- round(cor(vexp), 2)
 
+plot_correlation <- ggcorrplot(
+  corr,
+  hc.order = TRUE, lab = T, 
+  type = "lower",
+  outline.color = "white",
+  ggtheme = ggplot2::theme_bw,
+  colors = c("#0041A6", "white", "#A60000"))+
+  scale_y_discrete(position = "right")+
+  theme(panel.grid = element_blank(),
+        axis.line = element_line(color = 'black', linewidth =.2),
+        panel.border = element_blank(),
+        legend.position = c(0.2,0.7),
+        legend.key.size=unit(1, "cm"),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 12))
+plot_correlation
+
+
+ggsave("correlation.jpg",plot_correlation, dpi = 900, units = "cm", width = 16, height =16, limitsize = F)
 
 
